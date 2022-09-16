236aaedeef2c4096770dfba1aed0d9c815188445
liufuxiang <842110796@qq.com>
2022-09-16 13:45:45 +0800

    fix: 删除多余配置

--- src/main.js
+++ src/main.js
@@ -1,22 +1,11 @@
 import Vue from 'vue';
 
-import '@/plugins/request';
-import components from '@/plugins/components';
-
 import App from './App';
 import router from './router';
-import store from './store';
-
-// 按需引入组件，删除不需要的组件
-import '@/plugins/element-ui';
-import '@/styles/common.scss';
-
-Vue.use(components);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
